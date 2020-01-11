fdc92be5c571b863dbf3b6a621926d7a8cec5807
Vadim Bratskyi <vadymbratskyi@gmail.com>
2020-01-11 19:31:15 +0200

    Neuron

---
+++ ParserIntoTheMongoDB/NeuronLibrary/Models/Neuron.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuronLibrary.Models
+{
+	public class Neuron
+	{
+		public List<double> Weight { get; set; }
+		public NeuronType Type { get; set; }		
+		public double Output { get; private set; }
+		public Neuron(int inputCount, NeuronType type = NeuronType.Normal) {
+			Type = type;
+			Weight = new List<double>();
+
+			for (int i = 0; i < inputCount; i++)
+			{
+				Weight.Add(1);
+			}
+		}
+
+		/**
+		 *check input signal ***???
+		 */
+		public double FeedForward(List<double> inputs) {
+			var sum = 0.0;
+			for (int i = 0; i < inputs.Count; i++)
+			{
+				sum = +inputs[i] * Weight[i];
+			}
+			Output = Sigmoid(sum);
+
+
+			return sum;
+		}
+
+		private double Sigmoid(double x) {
+			return 1.0 / (1.0 + Math.Pow(Math.E, -x));
+		}
+
+		public override string ToString()
+		{
+			return Output.ToString();
+		}
+
+	}
+}
---
+++ ParserIntoTheMongoDB/NeuronLibrary/Models/NeuronType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuronLibrary.Models
+{
+	public enum NeuronType
+	{
+		Input = 0,
+		Normal = 1,
+		Output = 2
+	}
+}
