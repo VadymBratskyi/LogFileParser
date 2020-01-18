c221b8e6d2683580462ecd33c971d9d2dcc648bf
Vadim Bratskyi <vadymbratskyi@gmail.com>
2020-01-18 19:42:36 +0200

    added normalization and scalling

--- ParserIntoTheMongoDB/NeuraNetworks/Program.cs
+++ ParserIntoTheMongoDB/NeuraNetworks/Program.cs
@@ -11,48 +11,76 @@ namespace NeuraNetworks
 			Console.WriteLine("Hello Neuro Network!");
 			Console.WriteLine("Human is sick-1");
 			Console.WriteLine("Human is helthi-0");
-			//Result -- Human is sick-1
-			//			Human is helthi-0
+			//Result-- Human is sick - 1
+			//			Human is helthi - 0
 
-			//Temprache	T
-			//Good year	A
-			//Smoking	S
-			//Food helthi	F
 
-			var dataset = new List<Tuple<double, double[]>> { 
-			//												T	A	S	F
-				new Tuple<double, double[]>(0, new double[] { 0, 0, 0, 0}),
-				new Tuple<double, double[]>(0, new double[] { 0, 0, 0, 1}),
-				new Tuple<double, double[]>(1, new double[] { 0, 0, 1, 0}),
-				new Tuple<double, double[]>(0, new double[] { 0, 0, 1, 1}),
-				new Tuple<double, double[]>(0, new double[] { 0, 1, 0, 0}),
-				new Tuple<double, double[]>(0, new double[] { 0, 1, 0, 1}),
-				new Tuple<double, double[]>(1, new double[] { 0, 1, 1, 0}),
-				new Tuple<double, double[]>(0, new double[] { 0, 1, 1, 1}),
-				new Tuple<double, double[]>(1, new double[] { 1, 0, 0, 0}),
-				new Tuple<double, double[]>(1, new double[] { 1, 0, 0, 1}),
-				new Tuple<double, double[]>(1, new double[] { 1, 0, 1, 0}),
-				new Tuple<double, double[]>(1, new double[] { 1, 0, 1, 1}),
-				new Tuple<double, double[]>(1, new double[] { 1, 1, 0, 0}),
-				new Tuple<double, double[]>(0, new double[] { 1, 1, 0, 1}),
-				new Tuple<double, double[]>(1, new double[] { 1, 1, 1, 0}),
-				new Tuple<double, double[]>(1, new double[] { 1, 1, 1, 1}),
+			//Temprache T
+			//Good year   A
+			//Smoking S
+			//Food helthi F
+
+			//var dataset = new List<Tuple<double, double[]>> { 
+			////												T	A	S	F
+			//	new Tuple<double, double[]>(0, new double[] { 0, 0, 0, 0}),
+			//	new Tuple<double, double[]>(0, new double[] { 0, 0, 0, 1}),
+			//	new Tuple<double, double[]>(1, new double[] { 0, 0, 1, 0}),
+			//	new Tuple<double, double[]>(0, new double[] { 0, 0, 1, 1}),
+			//	new Tuple<double, double[]>(0, new double[] { 0, 1, 0, 0}),
+			//	new Tuple<double, double[]>(0, new double[] { 0, 1, 0, 1}),
+			//	new Tuple<double, double[]>(1, new double[] { 0, 1, 1, 0}),
+			//	new Tuple<double, double[]>(0, new double[] { 0, 1, 1, 1}),
+			//	new Tuple<double, double[]>(1, new double[] { 1, 0, 0, 0}),
+			//	new Tuple<double, double[]>(1, new double[] { 1, 0, 0, 1}),
+			//	new Tuple<double, double[]>(1, new double[] { 1, 0, 1, 0}),
+			//	new Tuple<double, double[]>(1, new double[] { 1, 0, 1, 1}),
+			//	new Tuple<double, double[]>(1, new double[] { 1, 1, 0, 0}),
+			//	new Tuple<double, double[]>(0, new double[] { 1, 1, 0, 1}),
+			//	new Tuple<double, double[]>(1, new double[] { 1, 1, 1, 0}),
+			//	new Tuple<double, double[]>(1, new double[] { 1, 1, 1, 1}),
+			//};
+
+			var outputs = new double[] { 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 };
+			var inputs = new double[,] {
+				{ 0, 0, 0, 0},
+				{ 0, 0, 0, 1},
+				{ 0, 0, 1, 0},
+				{ 0, 0, 1, 1},
+				{ 0, 1, 0, 0},
+				{ 0, 1, 0, 1},
+				{ 0, 1, 1, 0},
+				{ 0, 1, 1, 1},
+				{ 1, 0, 0, 0},
+				{ 1, 0, 0, 1},
+				{ 1, 0, 1, 0},
+				{ 1, 0, 1, 1},
+				{ 1, 1, 0, 0},
+				{ 1, 1, 0, 1},
+				{ 1, 1, 1, 0},
+				{ 1, 1, 1, 1}
 			};
 
 			var topology = new Topology(4, 1, 0.1, 2);
 			var neuronNetwork = new NeuronNetworks(topology);
-			var difference = neuronNetwork.Learning(dataset, 100000);
+			//var difference = neuronNetwork.Learning(dataset, 100000);
+			var difference = neuronNetwork.Learning(outputs, inputs, 100000);
 
 			var result = new List<double>();
-			foreach (var data in dataset)
+			for (int i = 0; i < outputs.Length; i++)
 			{
-				result.Add(neuronNetwork.FeedForward(data.Item2).Output);
+				var row = NeuronNetworks.GetRow(inputs, i);
+				result.Add(neuronNetwork.FeedForward(row).Output);
 			}
+			//foreach (var data in dataset)
+			//{
+			//	result.Add(neuronNetwork.FeedForward(data).Output);
+			//}
+
 			for (int i = 0; i < result.Count; i++)
 			{
-				var expecte = Math.Round(dataset[i].Item1, 3);
+				var expecte = Math.Round(outputs[i], 3);
 				var actual = Math.Round(result[i], 3);
-				Console.WriteLine(expecte + " -- " +actual);
+				Console.WriteLine(expecte + " -- " + actual);
 			}
 			Console.ReadKey();
 		}
--- ParserIntoTheMongoDB/NeuronLibrary/NeuronNetworks.cs
+++ ParserIntoTheMongoDB/NeuronLibrary/NeuronNetworks.cs
@@ -44,16 +44,101 @@ namespace NeuronLibrary
 		<param name="dataset">Data for learning</param>
 		<param name="epoch">Count cikle for sdudy. Count times to repead study</param>
 		 */
-		public double Learning(List<Tuple<double, double[]>> dataset, int epoch) {
+		//public double Learning(List<Tuple<double, double[]>> dataset, int epoch) {
+		//	var error = 0.0;
+		//	for (int i = 0; i < epoch; i++)
+		//	{ 
+		//		foreach (var data in dataset)
+		//		{
+		//			error += BackPropagation(data.Item1, data.Item2);
+		//		}
+		//	}
+		//	return error / epoch;
+		//}
+
+		public double Learning(double[] expected, double[,] inputs, int epoch)
+		{
 			var error = 0.0;
 			for (int i = 0; i < epoch; i++)
-			{ 
-				foreach (var data in dataset)
+			{
+				for (int j = 0; j < expected.Length; j++)
 				{
-					error += BackPropagation(data.Item1, data.Item2);
+					var output = expected[j];
+					var input = GetRow(inputs, j);
+					error += BackPropagation(output, input);
+				}
+			}			
+			return error / epoch;
+		}
+		public static double[] GetRow(double[,] matrix, int row) {
+			var columns = matrix.GetLength(1);
+			var array = new double[columns];
+			for (int i = 0; i < columns; ++i)
+			{
+				array[i] = matrix[row, i];
+			}
+			return array;
+		}
+		/**
+		 <summary>
+			Algoritm mashtabing
+		 </summary>
+		 */
+		private double[,] Scalling(double[,] inputs) {
+			var result = new double[inputs.GetLength(0), inputs.GetLength(1)];
+			for (int column = 0; column < inputs.GetLength(1); column++)
+			{
+				var min = inputs[0, column];
+				var max = inputs[0, column];
+
+				for (int row = 0; row < inputs.GetLength(0); row++)
+				{
+					var item = inputs[row, column];
+					if (item < min) {
+						min = item;
+					}
+					if (item > max) {
+						max = item;
+					}
+				}
+				var divider = max - min;
+				for (int row = 0; row < inputs.GetLength(0); row++)
+				{
+					result[row, column] = (inputs[row, column] - min) / divider;
 				}
 			}
-			return error / epoch;
+			return result;
+		}
+		/**
+		  <summary>
+			Algoritm normalization
+		 </summary>
+		 */
+		private double[,] Normalization(double [,] inputs) {
+			var result = new double[inputs.GetLength(0), inputs.GetLength(1)];
+			for (int column = 0; column < inputs.GetLength(1); column++)
+			{
+				//average value neuron signal
+				var sum = 0.0;
+				for (int row = 0; row < inputs.GetLength(0); row++)
+				{
+					sum += inputs[row, column];
+				}
+				var average = sum / inputs.GetLength(0);
+				//Standart kvadrat otklonenie neurona
+				var error = 0.0;
+				for (int row = 0; row < inputs.GetLength(0); row++)
+				{
+					error += Math.Pow((inputs[row, column] - average), 2);
+				}
+				var standartError = Math.Sqrt(error / inputs.GetLength(0));
+				//set new value for neuron
+				for (int row = 0; row < inputs.GetLength(0); row++)
+				{
+					result[row, column] = (inputs[row, column] - average) / standartError;
+				}
+			}
+			return result;
 		}
 		/**
 		 <summary>return error</summary>
